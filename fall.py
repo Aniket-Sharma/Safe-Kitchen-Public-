918b40a9cf1836885d9505e6a4df5c92fcd97934
Aniket Sharma <abcd13017@gmail.com>
2019-12-08 16:09:35 +0530

    Added fall detection system .

--- fall.py
+++ fall.py
@@ -3,6 +3,7 @@ import io
 import os
 import cv2
 import math
+import time 
 
 import imutils
 from imutils.object_detection import non_max_suppression
@@ -18,6 +19,9 @@ from google.cloud.vision import types
 
 import numpy as np
 import tensorflow as tf
+
+fgbg = cv2.createBackgroundSubtractorMOG2()
+j = 0
 
 # Opencv pre-trained SVM with HOG people features 
 
@@ -35,7 +39,56 @@ class Alarms :
     def Object():
         print('ALERT ! There is something unsual on the floor, watch your step . ')
 
-#detecting the persons present in the room. 
+#detecting fall 
+def fall_detector(frame):
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    fgmask = fgbg.apply(gray)
+    
+    #Find contours
+    contours,_  = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        areas = []
+
+        for contour in contours:
+            ar = cv2.contourArea(contour)
+            areas.append(ar)
+        
+        max_area = max(areas or [0])
+
+        max_area_index = areas.index(max_area)
+
+        cnt = contours[max_area_index]
+
+        M = cv2.moments(cnt)
+        
+        x, y, w, h = cv2.boundingRect(cnt)
+
+        cv2.drawContours(fgmask, [cnt], 0, (255,255,255), 3, maxLevel = 0)
+        
+        if h < w:
+            # j += 1
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.imshow('Fall detected', frame)
+            cv2.waitKey(150)
+            return True
+            
+        # if j > 10:
+        #     #print "FALL"
+        #     #cv2.putText(fgmask, 'FALL', (x, y), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255,255,255), 2)
+        #     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+        #     Alarms.Fall()
+
+        if h > w:
+            # j = 0 
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.imshow('person seems to be alright. ', frame)
+            cv2.waitKey(50)
+            return False
+        
+#detecting the persons present in the room.
+        
 def person_detector(image):
     print('checking if there\'s someone in the room ............')
     # retruns the reactangle array highlithing the location of person in given image.  
@@ -111,11 +164,13 @@ def fire_in_image():
     print('You are in option to detect fire in an image.')
     file_path = filedialog.askopenfilename()
     content = cv2.imread(file_path)
+    image = content.copy()
     content = imutils.resize(content, width=min(400, content.shape[1]))
     cv2.imshow("Original Image : ", content)
     
     persons_in_room = person_detector(content)
     # persons_in_room = person_detector_temp(frame)
+
     mark_person(content, persons_in_room)
     cv2.waitKey(600)
 
@@ -123,6 +178,17 @@ def fire_in_image():
 
     label4 = Label(leftFrame, text="Number of Persons in the Image : "+str(len(persons_in_room)), fg="Green", font=('Verdana', 15, 'bold'))
     label4.pack()
+
+    label7 = Label(leftFrame, text="Checking if the person has fallen or not . ... ", fg="Green", font=('Verdana', 15, 'bold'))
+    label7.pack()
+
+    if (fall_detector(image)):
+    	label8 = Label(leftFrame, text="The person might have fallen ... ", fg="Green", font=('Verdana', 15, 'bold'))
+    	label8.pack()
+        
+    else :
+    	label7 = Label(leftFrame, text="Person seems to be doing alright. ", fg="Green", font=('Verdana', 15, 'bold'))
+    	label7.pack()
 
     if (FIF_IP(content)):
 
@@ -145,6 +211,7 @@ def fire_in_image():
     return
     
 def fire_in_video():
+    Frames_with_Fire = []
     print('You are in option to detect fire in a video.')
     video_path = filedialog.askopenfilename()
     # with io.open(video_path, 'rb') as video_file:
@@ -162,7 +229,7 @@ def fire_in_video():
         frame_count+=1
         # will use the algorithm on every 100th frame
 
-        if frame_count>=4:
+        if frame_count>=1:
 
             cv2.putText(img=frame, text='checking if there\'s someone in the room ............' , org=(10, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(0, 0, 0))
             cv2.imshow("Live Video : ", frame)
@@ -205,6 +272,19 @@ def fire_in_video():
                 else :
                     Frames_with_Fire = [] 
 
+            else :
+            	print('Detecting fall .... ')
+            	frames_with_fall = 0 
+            	if (fall_detector(frame)):
+            		frames_with_fall+=1
+            		print('The person seems to be fallen, please stand up quickly to avoid the alarm. ')
+            		if frames_with_fall > 5:
+            			print('PLease be patient, your emergency contacts will be notified. ')
+            			Alarms.Fall()
+            		else :
+            		    frames_with_fall = 0 
+
+
             frame_count = 0
 
 
@@ -215,6 +295,7 @@ def fire_in_video():
         
 
 def fire_in_footage():
+
     Frames_with_Fire = []
     print('You are in option to detect fire in live vedio. ')
     sec = 1
@@ -227,7 +308,7 @@ def fire_in_footage():
         (grabbed, frame) = video.read()
         frame_count+=1
         # will use the algorithm on every 100th frame
-        if frame_count>=4:
+        if frame_count>=2:
             cv2.putText(img=frame, text='checking if there\'s someone in the room ............' , org=(10, 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(0, 0, 0))
             cv2.imshow("Live Video : ", frame)
 
@@ -267,7 +348,19 @@ def fire_in_footage():
                 else :
                     Frames_with_Fire = [] 
 
-            frame_count = 0 
+            else:
+            	print('Detecting fall .... ')
+            	frames_with_fall = 0 
+            	if (fall_detector(frame)):
+            		frames_with_fall+=1
+            		print('The person seems to be fallen, please stand up quickly to avoid the alarm. ')
+            		if frames_with_fall > 5:
+            			print('PLease be patient, your emergency contacts will be notified. ')
+            			Alarms.Fall()
+            		else :
+            		    frames_with_fall = 0  
+
+            frame_count = 0
 
         if not grabbed:
             print('not grabbed')
@@ -282,18 +375,18 @@ def fire_in_footage():
 if __name__=='__main__':
 
     root = tk.Tk()
-    root.title('Fire Detection App')
+    root.title('Fire and Fall Detection App')
     root.geometry("720x580")
     # Create 2 frames for interface
     leftFrame = Frame(root)
-    label2 = Label(leftFrame, text="Fire Detection System. ",fg="Red",  font=('Verdana', 25, 'bold'))
+    label2 = Label(leftFrame, text="Fire and Fall Detection System. ",fg="Red",  font=('Verdana', 25, 'bold'))
     label2.pack()
     quote = """
     We are trying to keep your loved ones safe from hazards \nthat are frequent reasons of injuries and loss among\n elederly people. 
     \n You can do other works while leaving your\n loved ones in our system\'s safe hands. """
     label0 = Label(leftFrame, text=quote,fg="Green", font=('Verdana', 15, 'bold'))
     label0.pack()
-    label1 = Label(leftFrame, text="\n\nSelect one of the following options : \n Check fire in a : \n ", font=('Verdana', 15, 'bold'))
+    label1 = Label(leftFrame, text="\n\nSelect one of the following options : \n Check fire/fall in a : \n ", font=('Verdana', 15, 'bold'))
     label1.pack()
     buttonSeek = Button(leftFrame, text="Picture", fg="Blue", bg="#DBF7DD",width=20, padx=20, pady=10 , font=('Rakkas', 22, 'bold', 'italic'), command=lambda : fire_in_image())
     buttonSeek.pack(side=LEFT)
